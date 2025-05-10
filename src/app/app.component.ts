656c1bae67960cb7137ea2a4463b2655dc811c78
JosuePaiva02 <u202119095@upc.edu.pe>
2025-05-09 20:08:24 -0500

    feat: set up navigation service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
 import { OnInit, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
