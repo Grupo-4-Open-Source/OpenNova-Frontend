dd61a5050fd7e78667e84ba77a56d0b969fe523d
Kenyi15upc <u202220138@upc.edu.pe>
2025-05-11 15:33:01 -0500

    feat: created my-post-cards component

---
+++ src/app/dashboard/components/my-post-cards/my-post-cards.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { MyPostService } from '../../services/my-post.service';
+import { PublishedVehicles } from '../../../navigation/model/published-vehicles.entity';
+import { CurrencyPipe, NgForOf } from '@angular/common';
+import {
+  MatCard,
+  MatCardActions,
+  MatCardContent,
+  MatCardHeader,
+  MatCardImage,
+  MatCardTitle
+} from '@angular/material/card';
+
+@Component({
+  selector: 'app-my-post-cards',
+  templateUrl: './my-post-cards.component.html',
+  imports: [
+    CurrencyPipe,
+    MatCard,
+    MatCardActions,
+    MatCardContent,
+    MatCardHeader,
+    MatCardImage,
+    MatCardTitle,
+    NgForOf
+  ],
+  styleUrls: ['./my-post-cards.component.css']
+})
+export class MyPostCardsComponent implements OnInit {
+  vehicles: PublishedVehicles[] = [];
+  displayedVehicles: PublishedVehicles[] = [];
+  limit = 4;
+
+  constructor(private myPostService: MyPostService) {}
+
+  ngOnInit(): void {
+    this.getMyPostVehicles();
+  }
+
+  private getMyPostVehicles(): void {
+    this.myPostService.getAll().subscribe((response: any) => {
+      this.vehicles = response;
+      this.displayedVehicles = this.vehicles.reverse().slice(0, this.limit);
+    });
+  }
+
+  showAll(): void {
+    this.displayedVehicles = this.vehicles.reverse();
+  }
+}
