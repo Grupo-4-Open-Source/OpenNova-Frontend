ddfc72bb45c42585b820832d3f7e739b48421d3a
JosuePaiva02 <u202119095@upc.edu.pe>
2025-05-12 23:21:31 -0500

    feat: enhanced toolbar and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,13 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from "./public/pages/home/home.component";
 import { AboutComponent } from "./public/pages/about/about.component";
 import { PageNotFoundComponent } from "./public/pages/page-not-found/page-not-found.component";
 import {NavigationViewComponent} from './navigation/pages/navigation-view/navigation-interface.component';
 import {DashboardComponent} from './dashboard/components/dashboard.component';
 
 export const routes: Routes = [
-  { path: 'home', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent },
   { path: 'about', component: AboutComponent },
   { path: 'navigation', component: NavigationViewComponent }, // Nueva ruta añadida
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ];
