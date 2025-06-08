f830ed93a57e3d189ca98568f0a68df0df06c310
Kenyi15upc <u202220138@upc.edu.pe>
2025-06-08 03:47:26 -0500

    feat(publications): add publish-vehicle component

--- src/app/dashboard/pages/renter-dashboard/renter-dashboard.component.ts
+++ src/app/dashboard/pages/renter-dashboard/renter-dashboard.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
-import { FeaturedVehiclesSectionComponent} from './featured-vehicles-section/featured-vehicles-section.component';
-import { BookedVehiclesSectionComponent} from './booked-vehicles-section/booked-vehicles-section.component';
-import { AvailableVehiclesSectionComponent} from './available-vehicles-section/available-vehicles-section.component';
+import { FeaturedVehiclesSectionComponent} from '../../components/renter-dashboard/featured-vehicles-section/featured-vehicles-section.component';
+import { BookedVehiclesSectionComponent} from '../../components/renter-dashboard/booked-vehicles-section/booked-vehicles-section.component';
+import { AvailableVehiclesSectionComponent} from '../../components/renter-dashboard/available-vehicles-section/available-vehicles-section.component';
 
 @Component({
   selector: 'app-renter-dashboard',
