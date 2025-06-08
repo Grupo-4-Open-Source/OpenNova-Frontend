052e5889fc252f2def7b53c0bf00609534a54c2b
Kenyi15upc <u202220138@upc.edu.pe>
2025-06-07 23:38:41 -0500

    feat: update environments

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,14 @@
 export const environment = {
   production: false,
 // Server Base Path for Fake REST API
-  serverBasePath: 'https://6822faa8b342dce800501fac.mockapi.io',
+// serverBasePath: 'https://6822faa8b342dce800501fac.mockapi.io',
+  serverBasePath: 'http://localhost:3000',
 // Server Base Path for Spring Boot REST API
 //serverBasePath: 'http://localhost:8090/api/v1',
-  vehiclesEndpointPath: '/vehicles'
+  vehiclesEndpointPath: '/vehicle',
+  publicacionesEndpointPath: '/publication',
+  locationsEndpointPath: '/location',
+  rentalsEndpointPath: '/rental',
+  usersEndpointPath: '/user',
+  insurancesEndpointPath: '/insurance'
 };
