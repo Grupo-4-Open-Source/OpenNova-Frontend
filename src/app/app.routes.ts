428b1799ca4f5cd62e6556a351af5ff252f4b49e
Kenyi15upc <u202220138@upc.edu.pe>
2025-06-08 04:04:52 -0500

    feat: add new routes (dashboard, publish-vehicle, rent-vehicle/publicationId)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,18 @@ import { Routes } from '@angular/router';
 import { AboutComponent } from "./public/pages/about/about.component";
 import { PageNotFoundComponent } from "./public/pages/page-not-found/page-not-found.component";
 import {NavigationViewComponent} from './navigation/pages/navigation-view/navigation-interface.component';
-import {DashboardComponent} from './dashboard/components/dashboard.component';
 import {VehicleManagementComponent} from './publications/pages/vehicle-management/vehicle-management.component';
+import { RenterDashboardComponent} from './dashboard/pages/renter-dashboard/renter-dashboard.component';
+import { PublishVehicleComponent} from './publications/pages/publish-vehicle/publish-vehicle.component';
+import { RentVehicleComponent} from './rental/pages/rent-vehicle.component';
 
 export const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard', component: RenterDashboardComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'navigation', component: NavigationViewComponent }, // Nueva ruta añadida
+  { path: 'navigation', component: NavigationViewComponent },
   {path: 'publications', component: VehicleManagementComponent},
+  { path: 'publish-vehicle', component: PublishVehicleComponent },
+  { path: 'rent-vehicle/:publicacionId', component: RentVehicleComponent },
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ];
